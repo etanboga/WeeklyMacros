03e7ae472d194f3974a72203b48ea432b9bc42ff
Ege Tanboga <ege72282@gmail.com>
2018-03-24 18:33:22 -0700

    Added screen to enter daily macros

---
+++ WeeklyMacros/View Controllers/EnterTodaysMacrosVC.swift
@@ -0,0 +1,18 @@
+//
+//  EnterTodaysMacrosVC.swift
+//  WeeklyMacros
+//
+//  Created by Ege Tanboga on 3/24/18.
+//  Copyright © 2018 Tanbooz. All rights reserved.
+//
+
+import UIKit
+
+class EnterTodaysMacrosVC : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
